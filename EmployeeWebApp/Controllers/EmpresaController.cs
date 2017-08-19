2473317cd4e86258bb69a994e08cc9018a85619c
vinicius nunes <viniiciuspereiira@hotmail.com>
2017-08-19 12:46:17 -0300

    Index e Create em funcionamento

---
+++ EmployeeWebApp/Controllers/EmpresaController.cs
@@ -0,0 +1,74 @@
+﻿using EmployeeWebApp.Models;
+using EmployeeWebApp.Repository.Context;
+using EmployeeWebApp.Repository.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeWebApp.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        private DataContext contexto = new DataContext();
+        private EmpresaService service = new EmpresaService(new DataContext());
+
+        // GET: Empresa
+        public ActionResult Index()
+        {
+            return View(service.GetEmpresas());
+        }
+
+        // GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // GET
+        public ActionResult Edit(Empresa empresa, int id)
+        {
+            var query = (from e in contexto.Empresas
+                         where e.EmpresaID == id
+                         select e).FirstOrDefault();
+            return View(query);
+        }
+        
+        // GET
+        public ActionResult Delete(Empresa empresa, int id)
+        {
+            var query = (from e in contexto.Empresas
+                         where e.EmpresaID == id
+                         select e).FirstOrDefault();
+            return View(query);
+        }
+        
+        // GET
+        public ActionResult Details(Empresa empresa, int id)
+        {
+            var query = (from e in contexto.Empresas
+                         where e.EmpresaID == id
+                         select e).FirstOrDefault();
+            return View(query);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Empresa empresa)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    service.CreateEmpresa(empresa);
+                    return RedirectToAction("Index");
+                }
+            }
+            catch 
+            {
+                ModelState.AddModelError("", "Tente novamente");
+            }
+            return View();
+        }
+    }
+}
---
+++ EmployeeWebApp/Controllers/FuncionarioController.cs
@@ -0,0 +1,77 @@
+﻿using EmployeeWebApp.Models;
+using EmployeeWebApp.Repository.Context;
+using EmployeeWebApp.Repository.Service;
+using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeWebApp.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        private DataContext contexto = new DataContext();
+        private FuncionarioService service = new FuncionarioService(new DataContext());
+
+        // GET: Funcionario
+        public ActionResult Index()
+        {
+            var funcionarios = contexto.Funcionarios.Include(f => f.Empresa);
+            return View(funcionarios.ToList());
+        }
+
+        // GET
+        public ActionResult Create()
+        {
+            ViewBag.EmpresaID = new SelectList(contexto.Empresas, "EmpresaID", "NomeDaEmpresa");
+            return View();
+        }
+
+        // GET
+        public ActionResult Edit(Funcionario funcionario, int id)
+        {
+            var query = (from f in contexto.Funcionarios
+                         where f.FuncionarioID == id
+                         select f).FirstOrDefault();
+            return View(query);
+        }
+
+        // GET
+        public ActionResult Delete(Funcionario funcionario, int id)
+        {
+            var query = (from f in contexto.Funcionarios
+                         where f.FuncionarioID == id
+                         select f).FirstOrDefault();
+            return View(query);
+        }
+
+        // GET
+        public ActionResult Details(Funcionario funcionario, int id)
+        {
+            var query = (from f in contexto.Funcionarios
+                         where f.FuncionarioID == id
+                         select f).FirstOrDefault();
+            return View(query);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Funcionario funcionario)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    service.CreateFuncionario(funcionario);
+                    return RedirectToAction("Index");
+                }
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Tente novamente");
+            }
+            return View();
+        }
+    }
+}
